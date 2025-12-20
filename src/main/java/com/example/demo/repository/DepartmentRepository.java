3ef11fb99b8b5f1d04470cd212e83cb18787683e
bala-amypo <bala@amypo.in>
2025-12-20 12:02:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Department;
+import com.example.demo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByName(String name);
+
     boolean existsByName(String name);
 }
