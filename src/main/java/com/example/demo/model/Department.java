220ec01a5d1de2b0829c5776a1c0051c3ecc395d
bala-amypo <bala@amypo.in>
2025-12-20 09:54:50 +0000

    User commit

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -2,7 +2,6 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import java.util.List;
 
 @Entity
 public class Department {
@@ -14,25 +13,19 @@ public class Department {
     @NotBlank(message = "Department name is required")
     private String name;
 
-    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
-    private List<ShiftTemplate> shiftTemplates;
-
-    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
-    private List<GeneratedShiftSchedule> schedules;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public List<ShiftTemplate> getShiftTemplates() { return shiftTemplates; }
-    public void setShiftTemplates(List<ShiftTemplate> shiftTemplates) {
-        this.shiftTemplates = shiftTemplates;
+    public Long getId() {
+        return id;
     }
 
-    public List<GeneratedShiftSchedule> getSchedules() { return schedules; }
-    public void setSchedules(List<GeneratedShiftSchedule> schedules) {
-        this.schedules = schedules;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
