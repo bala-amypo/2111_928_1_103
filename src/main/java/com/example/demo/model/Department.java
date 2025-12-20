37d34e62f818c35027dab8dca4858c4712df02a3
bala-amypo <bala@amypo.in>
2025-12-20 12:37:09 +0000

    User commit

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(
-        name = "departments",
+        name = "department",
         uniqueConstraints = @UniqueConstraint(columnNames = "name")
 )
 public class Department {
