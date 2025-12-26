a5f4073d58712446a019c041beb6f015e0c442e6
bala-amypo <bala@amypo.in>
2025-12-26 06:38:08 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .servers(List.of(
-                        new Server().url("https://9302.pro604cr.amypo.ai/")
+                        new Server().url("https://9125.408procr.amypo.ai/")
                 ));
     }
 }
